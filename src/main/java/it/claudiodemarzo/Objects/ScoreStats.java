4096d5d14522ae97bef04c520277cd45b00ca236
Claudio <claudiodemarzo.62@gmail.com>
2021-07-04 12:34:50 +0200

    added ScoreStats

--- src/main/java/it/claudiodemarzo/Objects/ScoreStats.java
+++ src/main/java/it/claudiodemarzo/Objects/ScoreStats.java
@@ -10,9 +10,43 @@ public class ScoreStats {
 
     private ScoreStats(float averageRankedAccuracy, int rankedPlayCount, int totalPlayCount, int totalRankedScore, int totalScore){
         this.averageRankedAccuracy = averageRankedAccuracy;
-        this.rankingPlayCount = rankingPlayCount;
+        this.rankedgPlayCount = rankedPlayCount;
         this.totalPlayCount = totalPlayCount;
         this.totalRankedScore = totalRankedScore;
         int totalScore = totalScore;
     }
+
+    public static ScoreStats fromJSONObject(JSONObject data) {
+        float averageRankedAccuracy = data.getFloat("averageRankedAccuracy");
+        int rankedPlayCount = data.getInt("rankedPlayCount");
+        int totalPlayCount = data.getInt("totalPlayCount");
+        int totalRankedScore = data.getInt("totalRankedScore");
+        int totalScore = data.getInt("totalScore");
+        return new ScoreStats(averageRankedAccuracy, rankedPlayCount, totalPlayCount, totalRankedScore, totalScore);
+    }
+
+    public static ScoreStats fromString(String data){
+        return fromJSONObject(new JSONObject(data));
+    }
+
+    public float getAverageRankedAccuracy() {
+        return averageRankedAccuracy;
+    }
+
+    public int getRankedPlayCount() {
+        return rankedPlayCount;
+    }
+
+    public int getTotalPlayCount() {
+        return totalPlayCount;
+    }
+
+    public int getTotalRankedScore() {
+        return totalRankedScore;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
 }
